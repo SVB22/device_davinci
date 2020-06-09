1a5ca0cb9cce20c575a37f2162e91d78ee887e91
mukesh22584 <mks22584@gmail.com>
2020-06-09 20:08:23 +0530

    sm8150-common:remove OnePlusDoze & enable CustomDoze

--- device.mk
+++ device.mk
@@ -155,6 +155,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+# Doze
+PRODUCT_PACKAGES += \
+    CustomDoze
+
 # DPM
 PRODUCT_PACKAGES += \
     libshim_dpmframework \
