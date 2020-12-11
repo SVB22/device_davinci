3be6a98b3f0139a35aeb04382fc9037a3c8c2b73
DennySPB <dennyspb@gmail.com>
2020-12-11 17:51:53 +0300

    Provide shim for libcameraservice.so

--- device.mk
+++ device.mk
@@ -108,10 +108,8 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor
 
 PRODUCT_PACKAGES += \
-    libshim_megvii
-
-PRODUCT_PACKAGES += \
-    Snap
+    libshim_megvii \
+	libmedia_jni_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
